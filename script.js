b34ff1a88b6a6fc4013b311096af8508b09eb757
Lea Bruneau <blea84130@gmail.com>
2023-10-04 17:58:59 +0200

    j'ai envoyé le script de l'exo 2

---
+++ script.js
@@ -0,0 +1,14 @@
+function validateForm() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  if (name === '' || email === '' || message === '') {
+    alert('Veuillez remplir tous les champs.');
+    return false;
+  }
+
+  // Vous pouvez ajouter ici un code pour soumettre le formulaire à un serveur.
+  alert('Formulaire soumis avec succès !');
+  return true;
+}
